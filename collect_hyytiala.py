b014734f6cf6451d3f897c2bd2db689b9133936b
samumantha <samantha.wittke@aalto.fi>
2022-04-25 15:45:17 +0300

    allas test

--- collect_hyytiala.py
+++ collect_hyytiala.py
@@ -8,7 +8,7 @@ import os
 import rasterio
 
 
-datadirs = ["/scratch/project_2005334/EODIE_process_akivimaki/Results/2020/tif","/scratch/project_2005334/EODIE_process_akivimaki/Results/2021/tif", "/scratch/project_2005334/EODIE_process_forest/EODIE_2021_results"]
+datadirs = ["/scratch/project_2005334/EODIE_process_akivimaki/Results/2020/tif","/scratch/project_2005334/EODIE_process_akivimaki/Results/2021/tif", "/scratch/project_2005334/EODIE_process_forest/EODIE_2021_results/tif"]
 startdate = 20200401
 enddate = 20210930
 tile = '34VFN'
@@ -56,3 +56,6 @@ print(len(listtokeep))
 with open('finalkeepers_hyytiala_all.txt', 'w') as f:
     for yeafile in listtokeep:
         f.write("%s\n" % yeafile)
+
+
+
